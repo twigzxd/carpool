2d321cd5e96898a8bb2dd914d2092c18243b874b
yang <y655li@uwaterloo.ca>
2018-05-14 12:26:57 -0400

    env set up

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,9 @@
+// module.exports = {
+//   mongoURI: "mongodb://mern:mern@ds249249.mlab.com:49249/mern",
+//   secretOrKey: "secret"
+// };
+
 module.exports = {
-  mongoURI: "mongodb://mern:mern@ds249249.mlab.com:49249/mern",
-  secretOrKey: "secret"
+    mongoURI: "mongodb://carpool:abcd1234@ds113630.mlab.com:13630/carpool",
+    secretOrKey: "secret"
 };
